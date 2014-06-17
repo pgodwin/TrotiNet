87254998b4f76ad39a05b966c2850e9eaa49aa88
Peter Godwin <pgodwin@gmail.com>
2014-06-18 09:22:24 +1000

    Added Header Names/Values & IProxyAuthenticator
    Still need to implement the Auth Stuff

---
+++ Lib/IProxyAuthenticator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrotiNet
+{
+    /// <summary>
+    /// Interface to allow for custom Authentication Implementations. 
+    /// </summary>
+    public interface IProxyAuthenticator
+    {
+        /// <summary>
+        /// Authenticates the user with the specified username/password
+        /// </summary>
+        /// <param name="username">Username passed from the browser</param>
+        /// <param name="password">Password passed from the browser</param>
+        /// <returns>true if the supplied credentials are correct, otherwise false.</returns>
+        bool Authenticate(string username, string password);
+    }
+}
